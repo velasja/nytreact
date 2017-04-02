e57ace25f3c982a842eb3e0cd9b8fc5f9efae9dc
JJ Velasquez <velas.ja@gmail.com>
2017-04-02 14:58:45 -0500

    initial files

---
+++ app/app.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Grabs the Routes
+var routes = require("./config/routes");
+
+// Renders the contents according to the route page.
+ReactDOM.render(routes, document.getElementById("app"));
